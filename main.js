0b6060f08bc91320b6137f61582d9d5cd18d8c36
wawerusamuel <waweruzamuel@gmail.com>
2022-07-25 14:55:46 +0300

    added the color display container...

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const button = document.querySelector("button");
 const div = document.querySelector(".color");
+const colorDiv = document.querySelector(".color-display h5");
 
 div.addEventListener("pointerover", changeBgColor);
 div.addEventListener("pointerout", revertBgColor);
@@ -10,11 +11,14 @@ function changeBgColor() {
   const randomRedValue = Math.floor(Math.random() * 255);
   const randomGreenValue = Math.floor(Math.random() * 255);
   const randomBlueValue = Math.floor(Math.random() * 255);
+  const color = `rgb(${randomRedValue}, ${randomGreenValue}, ${randomBlueValue})`;
 
   div.style.transition = "background-color .5s";
-  div.style.backgroundColor = `rgb(${randomRedValue}, ${randomGreenValue}, ${randomBlueValue})`;
+  div.style.backgroundColor = color;
+  colorDiv.innerText = color;
 }
 
 function revertBgColor() {
   div.style.backgroundColor = "black";
+  colorDiv.innerText = "rgb(0, 0, 0)";
 }
